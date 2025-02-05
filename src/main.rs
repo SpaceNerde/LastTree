94932382a291292bc8ddece98ae20f216cb26cbe
SpaceNerde <spacenerddev@gmail.com>
2025-02-05 15:42:12 +0100

    set up basic bevy project

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::WindowMode};
 
 fn main() {
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        mode: WindowMode::Windowed,
+                        fit_canvas_to_parent: false,
+                        title: "Last Tree".to_string(),
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .build(),
+        )
+        .run();
 }
